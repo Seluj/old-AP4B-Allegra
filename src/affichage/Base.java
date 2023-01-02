7f55aa3bc86ed3013a31652753f56c111e15a2d6
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-02 23:23:33 +0100

    ajout des attributs pour la tailles de les fenetres de jeu et de menu dans l'interface

--- src/affichage/Base.java
+++ src/affichage/Base.java
@@ -5,4 +5,11 @@ import javax.swing.*;
 public interface Base {
     JFrame frame = new JFrame();
     JPanel cards = new JPanel(); //a panel that uses CardLayout
+
+    int jeuWidth = 1280;
+    int jeuHeight = 720;
+
+    int menuWidth = 300;
+    int menuHeight = 500;
+
 }
