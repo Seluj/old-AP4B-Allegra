0d47eda1c9a1bf73e7b11e8f8a2dba13791326bf
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-03 12:23:46 +0100

    changement taille de la fenetre de jeu

--- src/Base.java
+++ src/Base.java
@@ -4,10 +4,9 @@ public interface Base {
     JFrame frame = new JFrame();
     JPanel cards = new JPanel(); //a panel that uses CardLayout
 
-    int jeuWidth = 1280;
-    int jeuHeight = 720;
+    int jeuWidth = 1980;
+    int jeuHeight = 1080;
 
     int menuWidth = 300;
     int menuHeight = 500;
-
 }
