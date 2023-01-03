f102bff64b3d8fa51c1c6e0a3b216794b8077e2a
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-03 19:42:20 +0100

    change display but not yet finish

--- src/Jeu.java
+++ src/Jeu.java
@@ -61,18 +61,39 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
 
     }
 
-    /*
-    public void printCard(Plateau p) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (p.getPlateau()[i][j] != null) {
-                    // TODO
+    public int[] toTable(int y) {
+        int[] tab = new int[2];
+        for (int i = 0; i < y; i++) {
+            tab[1] ++;
+            if (tab[1] == Y) {
+                tab[1] = 0;
+                tab[0] ++;
+            }
+        }
+        return tab;
+    }
+
+    public int toInterface(int x, int y) {
+        int tab;
+        tab = y + (x * 3 + x);
+        return tab;
+    }
+
+
+    public void printCard(Plateau p, int joueur) {
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < Y; j++) {
+                if (p.getCache(i, j) == 0) {
+                    bze[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/back.png"));
+                } else if (p.getCache(i, j) == 1) {
+                    bze[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/" + p.getCartes(i, j) + ".png"));
+                } else {
+                    bze[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/back.png"));
                 }
+                bze[joueur][toInterface(i, j)].paintComponent(bze[joueur][toInterface(i, j)].getGraphics());
             }
         }
     }
-
-     */
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -86,10 +107,14 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("click");
+        int tab[] = new int[2];
         for (int i = 0; i < bze.length; i++) {
             for (int j = 0; j < bze[i].length; j++) {
                 if (e.getSource() == bze[i][j]) {
                     System.out.println("i = " + i + " j = " + j + " clicked");
+                    tab = toTable(j);
+                    System.out.println("X = " + tab[0] + " Y = " + tab[1]);
+                    System.out.println("i = " + i + " Y = " + toInterface(tab[0], tab[1]));
                 }
             }
         }
@@ -116,7 +141,7 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
     }
 
 
-    class MyJLabel extends JLabel
+    static class MyJLabel extends JLabel
     {
         ImageIcon imageIcon;
         public MyJLabel(ImageIcon icon)
@@ -124,11 +149,23 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
             super();
             this.imageIcon = icon;
         }
+
+        public MyJLabel()
+        {
+            super();
+            this.imageIcon = null;
+        }
+
+        public void setIcon(ImageIcon imageIcon) {
+            this.imageIcon = imageIcon;
+        }
+
         @Override
         public void paintComponent(Graphics g)
         {
             super.paintComponent(g);
-            g.drawImage(imageIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
+            if (this.imageIcon != null)
+                g.drawImage(imageIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
         }
     }
 }
