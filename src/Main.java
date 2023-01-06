fdde947efb6670b480fa48873b3fc0712baf424f
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-06 16:25:48 +0100

    add login frame and pictures

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ public class Main {
 
         Menu menu = new Menu();
         Jeu jeu = new Jeu();
+        LoginJFrame login = new LoginJFrame();
 
 
         Fenetre f = new Fenetre(menu, jeu);
         Pioche p = new Pioche();
         System.out.println(p);
         Joueur j1 = new Joueur("Jules", p);
+        jeu.printCard(j1.getPlateau(), 0);
+        jeu.revalidate();
+
     }
 }
