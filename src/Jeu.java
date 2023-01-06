387b98a2b3997f9fc251e92fdd0b6a2dd17e3e92
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-06 15:04:51 +0100

    add comments and rename a variable to display_cards

--- src/Jeu.java
+++ src/Jeu.java
@@ -6,61 +6,90 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
-    MyJLabel bze[][] = new MyJLabel[6][12];
+    // Variables
+
+    // Tableau de cartes
+    // 12 cartes par joueur
+    // 6 joueurs
+    MyJLabel[][] display_cards = new MyJLabel[6][12];
+
+    // Table of JPanel to fil the grid.
+    // The grid is 5 raw and 5 column.
+    // 24 JPanels for game et 1 case for the menu button
     JPanel[] panels = new JPanel[24];
 
+    /**
+     * Build the game panel
+     *
+     */
     public Jeu() {
+        // Set basic information about the panel
         setLayout(new GridLayout(5,5));
+        setName("jeu");         // Set the name of the panel, it will be used to switch between panels
+        
+        // Set basic information about the frame
         frame.setSize(jeuWidth, jeuHeight);
         frame.setLocationRelativeTo(null);
-        setName("jeu");
-
+        
+        // Initialize the panels
         for (int i = 0; i < 24; i++) {
             panels[i] = new JPanel();
-
         }
+
+        // Panels 7, 11, 13 and 17 are for the cards of players.
+        // So we set those panels to display the cards with GridLayout and adding listener to the panels of cards
 
         panels[7].setLayout(new GridLayout(3,4, 5, 5));
 
         for (int i = 0; i < 12; i++) {
-            bze[0][i] = new MyJLabel();
-            bze[0][i].addMouseListener(this);
-            panels[7].add(bze[0][i]);
+            // Initialize all JLabels
+            display_cards[0][i] = new MyJLabel();
+            // Add a listener to the JLabel
+            display_cards[0][i].addMouseListener(this);
+            // And add the JLabel to the panel
+            panels[7].add(display_cards[0][i]);
         }
 
+        // Same for the other panels
         panels[11].setLayout(new GridLayout(3, 4, 5, 5));
         for (int i = 0; i < 12; i++) {
-            bze[1][i] = new MyJLabel();
-            bze[1][i].addMouseListener(this);
-            panels[11].add(bze[1][i]);
+            display_cards[1][i] = new MyJLabel();
+            display_cards[1][i].addMouseListener(this);
+            panels[11].add(display_cards[1][i]);
         }
 
         panels[13].setLayout(new GridLayout(3, 4, 5, 5));
         for (int i = 0; i < 12; i++) {
-            bze[2][i] = new MyJLabel();
-            bze[2][i].addMouseListener(this);
-            panels[13].add(bze[2][i]);
+            display_cards[2][i] = new MyJLabel();
+            display_cards[2][i].addMouseListener(this);
+            panels[13].add(display_cards[2][i]);
         }
 
         panels[17].setLayout(new GridLayout(3, 4, 5, 5));
         for (int i = 0; i < 12; i++) {
-            bze[3][i] = new MyJLabel();
-            bze[3][i].addMouseListener(this);
-            panels[17].add(bze[3][i]);
+            display_cards[3][i] = new MyJLabel();
+            display_cards[3][i].addMouseListener(this);
+            panels[17].add(display_cards[3][i]);
         }
 
-
-
-
+        // Add the panels to the grid
         for (int i = 0; i < 24; i++) {
             add(panels[i]);
         }
+
+        // Add the menu button and set the listener
         JButton b = new JButton("menu");
         b.addActionListener(this);
         add(b);
 
     }
 
+
+    /**
+     * Translate coordinates of the card pressed to the coordinates of the card in the table of the player
+     * @param y The y coordinate of the card pressed
+     * @return The x and y coordinates of the card in the table of the player
+     */
     public int[] toTable(int y) {
         int[] tab = new int[2];
         for (int i = 0; i < y; i++) {
@@ -73,24 +102,34 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
         return tab;
     }
 
+
+    /**
+     * Translate coordinates of the card in the table of the player to the coordinates of the card in the table of the game
+     * @param x The x coordinate of the card in the table of the player
+     * @param y The y coordinate of the card in the table of the player
+     * @return The y coordinates of the card in the table of the game
+     */
     public int toInterface(int x, int y) {
-        int tab;
-        tab = y + (x * 3 + x);
-        return tab;
+        return y + (x * 3 + x);
     }
 
 
+    /**
+     * Display the cards of the player
+     * @param p The cards of the player
+     * @param joueur The number of the player to know where to display the cards
+     */
     public void printCard(Plateau p, int joueur) {
         for (int i = 0; i < X; i++) {
             for (int j = 0; j < Y; j++) {
                 if (p.getCache(i, j) == 0) {
-                    bze[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/back.png"));
+                    display_cards[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/back.png"));
                 } else if (p.getCache(i, j) == 1) {
-                    bze[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/" + p.getCartes(i, j) + ".png"));
+                    display_cards[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/" + p.getCartes(i, j) + ".png"));
                 } else {
-                    bze[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/back.png"));
+                    display_cards[joueur][toInterface(i, j)].setIcon(new ImageIcon("src/images/back.png"));
                 }
-                bze[joueur][toInterface(i, j)].paintComponent(bze[joueur][toInterface(i, j)].getGraphics());
+                display_cards[joueur][toInterface(i, j)].paintComponent(display_cards[joueur][toInterface(i, j)].getGraphics());
             }
         }
     }
@@ -107,10 +146,10 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("click");
-        int tab[] = new int[2];
-        for (int i = 0; i < bze.length; i++) {
-            for (int j = 0; j < bze[i].length; j++) {
-                if (e.getSource() == bze[i][j]) {
+        int[] tab;
+        for (int i = 0; i < display_cards.length; i++) {
+            for (int j = 0; j < display_cards[i].length; j++) {
+                if (e.getSource() == display_cards[i][j]) {
                     System.out.println("i = " + i + " j = " + j + " clicked");
                     tab = toTable(j);
                     System.out.println("X = " + tab[0] + " Y = " + tab[1]);
@@ -140,7 +179,9 @@ public class Jeu extends JPanel implements Base, ActionListener, MouseListener {
 
     }
 
-
+    /**
+     * A subclass of JLabel to add a method to paint the component and resize the image
+     */
     static class MyJLabel extends JLabel
     {
         ImageIcon imageIcon;
