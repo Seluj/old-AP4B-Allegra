4ac82ceaf01f7534ecf9e5d4d2021825d4fe99b2
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-06 16:24:44 +0100

    add rules

--- src/Help.java
+++ src/Help.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -5,5 +6,14 @@ class Help implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("Help button pressed");
+        JDialog jDialog = new JDialog();
+        JLabel jLabel = new JLabel(new ImageIcon("src\\Images\\rule.png"));
+        jLabel.setBounds(0,0,599,727);
+        jDialog.getContentPane().add(jLabel);
+        jDialog.setSize(800,800);
+        jDialog.setAlwaysOnTop(true);
+        jDialog.setLocationRelativeTo(null);
+        jDialog.setModal(true);
+        jDialog.setVisible(true);
     }
 }
