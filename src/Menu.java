5274190ba1b4e8b67959d58ad3458fc4879c304d
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-07 11:57:50 +0100

    Change look

--- src/Menu.java
+++ src/Menu.java
@@ -4,39 +4,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Menu extends JPanel implements Base, ActionListener {
-    private JLabel name;
-    private JTextField tname;
-    private JLabel nbj;
+
     private JComboBox nbJoueurs;
     private JButton sub;
-
+    JTextField username = new JTextField();
     private int joueurs;
 
-    private String nb[]
-            = { "2", "3", "4", "5", "6"};
+    private String nb[] = { "2", "3", "4", "5", "6"};
+
     public Menu() {
         setName("menu");
-
         setLayout(null);
-        name = new JLabel("Name");
-        name.setSize(100, 20);
-        name.setLocation(100, 100);
-        add(name);
 
-        tname = new JTextField();
-        tname.setSize(190, 20);
-        tname.setLocation(200, 100);
-        add(tname);
+        JLabel usernameText = new JLabel(new ImageIcon("src\\Images\\login\\username.png"));
+        usernameText.setBounds(50, 135, 100, 23);
+        add(usernameText);
+        this.username.setBounds(200, 134, 150, 32);
+        add(this.username);
 
-
-        nbj = new JLabel("Nombre de joueurs");
-        nbj.setSize(120, 20);
-        nbj.setLocation(100, 250);
-        add(nbj);
+        JLabel passwordText = new JLabel(new ImageIcon("src\\Images\\login\\nb_joueur.png"));
+        passwordText.setBounds(50, 195, 100, 28);
+        add(passwordText);
 
         nbJoueurs = new JComboBox(nb);
-        nbJoueurs.setSize(50, 20);
-        nbJoueurs.setLocation(220, 250);
+        nbJoueurs.setBounds(200, 195, 150, 32);
         add(nbJoueurs);
 
         sub = new JButton("Submit");
@@ -44,15 +35,18 @@ public class Menu extends JPanel implements Base, ActionListener {
         sub.setLocation((menuWidth/2)-50, 300);
         sub.addActionListener(this);
         add(sub);
-        }
+        JLabel background = new JLabel(new ImageIcon("src\\Images\\login\\backgroun.jpg"));
+        background.setBounds(0, 0, 400, 514);
+        add(background);
+    }
+
 
     public int getJoueurs(){
         return joueurs;
     }
-
     /**
      * Function used to return the number of players selected
-      * @return number of players selected
+     * @return number of players selected
      */
     public void setNbJoueurs(){
         String choixJ = (String)nbJoueurs.getSelectedItem();
@@ -65,23 +59,13 @@ public class Menu extends JPanel implements Base, ActionListener {
         // We gather the info regarding the number of players selected
         setNbJoueurs();
         // Modify the frame size and location to fit the game
-        frame.setSize(jeuWidth, jeuHeight);
-        frame.setLocationRelativeTo(null);
         CardLayout cl = (CardLayout) (cards.getLayout());
         cl.show(cards, "jeu");
     }
 
-    public void paintComponent(Graphics g){
-        g.setColor(Color.BLACK);
-        g.drawRect(100,250,120,20);
-        g.drawLine(0, 0, this.getWidth(), this.getHeight());
-        g.drawLine(0, this.getHeight(), this.getWidth(), 0);
 
-        for (int i = 0; i < getWidth(); i+=10) {
-            g.drawLine(i, 0, i, this.getHeight());
-        }
-        for (int i = 0; i < getHeight(); i+=5) {
-            g.drawLine(0, i, this.getWidth(), i);
-        }
+    public void paintComponent(Graphics g) {
+        frame.setSize(menuWidth, menuHeight);
+        frame.setLocationRelativeTo(null);
     }
 }
