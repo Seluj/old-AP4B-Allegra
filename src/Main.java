6567033c4552b094b565a59a98a72d64baf634e8
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-15 16:18:48 +0100

    z

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 
     // ---------------------- CONSTRUCTOR ---------------------- //
-
+//aze
     public static void main(String[] args) {
 
         Controller controller = new Controller();
